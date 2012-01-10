427fda028aa8042e76d5bd35340e8ac6c8b20bfd
Jan Ahrens <jan.ahrens@googlemail.com>
2012-01-10 22:23:01 +0100

    xmonad: xmobar config and minortweaks

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,13 +1,25 @@
 import XMonad
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.DynamicLog
 import XMonad.Layout.NoBorders
 import XMonad.Util.EZConfig(additionalKeysP)
 import XMonad.Actions.Volume
+import XMonad.Util.Run(spawnPipe)
+
+import System.IO(hPutStrLn)
 
 main = do
+  xmproc <- spawnPipe "xmobar"
   xmonad $ defaultConfig {
-      manageHook =
+      terminal = "konsole"
+
+    , borderWidth        = 3
+    , normalBorderColor  = "#1E1C10"
+    , focusedBorderColor = "#5D0017"
+
+    , manageHook =
             manageDocks
 
         -- fix full screen layout for videos etc.
@@ -17,16 +29,21 @@ main = do
         <+> manageHook defaultConfig
 
     , layoutHook =
-          avoidStruts
 
         -- automatically remove borders from full screen layouts
-        $ smartBorders
+          smartBorders
 
         -- use default layoutHook
-        $ layoutHook defaultConfig
+        $ avoidStruts (layoutHook defaultConfig) ||| Full
 
     -- use the ,,windows key'' as mod key
     , modMask    = mod4Mask
+    , logHook    = dynamicLogWithPP $ xmobarPP
+                    { ppOutput = hPutStrLn xmproc
+                    , ppTitle = xmobarColor "#cdcd57" "" . shorten 50
+                    , ppCurrent = xmobarColor "#cdcd57" ""
+                    , ppSep = " <fc=#3d3d07>|</fc> "
+                    }
   } `additionalKeysP` [ ("<XF86AudioMute>", toggleMute >> return()),
     ("<XF86AudioLowerVolume>",  lowerVolume 4 >> return()),
     ("<XF86AudioRaiseVolume>",  raiseVolume 4 >> return())]
