a7cf2c432980c077968d85a4eea8849c768d31bd
Jan Ahrens <jan.ahrens+git@googlemail.com>
2013-07-31 21:23:22 +0200

    copied package.el install code from emacs-prelude and removed ergoemacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,13 +1,10 @@
-(require 'package)
-(add-to-list 'package-archives
-  '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives
-  '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(package-initialize)
+(add-to-list 'load-path (file-name-directory (or load-file-name buffer-file-name)))
+
+(load "init_packages")
 
 (load-theme 'deeper-blue)
-(set-default-font "Monospace-16")
-(set-frame-font "Monospace-16")
+(set-default-font "Monospace-14")
+(set-frame-font "Monospace-14")
 
 ;; disable some gui stuff
 (tool-bar-mode -1)
@@ -45,18 +42,3 @@
 
 ;; always start maximized
 (set-frame-parameter nil 'fullscreen 'maximized)
-
-;; uses standard ergoemacs keyboard theme
-(setq ergoemacs-theme nil)
-;; assumes QWERTY keyboard layout
-(setq ergoemacs-keyboard-layout "us")
-;; enable http://ergoemacs.github.io/ergoemacs-mode
-(ergoemacs-mode 1)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ergoemacs-mode-used "5.8.0")
- '(ergoemacs-theme nil))
---
+++ .emacs.d/init_packages.el
@@ -0,0 +1,40 @@
+;; this file was inspired and was largely copied from [prelude](http://batsov.com/prelude/):
+;; https://github.com/bbatsov/prelude/blob/8905997172ad07c792070152d3261aea173201a6/core/prelude-packages.el
+
+(require 'cl)
+(require 'package)
+
+(add-to-list 'package-archives '("tromey" . "http://tromey.com/elpa/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(package-initialize)
+
+(defvar prelude-packages
+  '(magit flycheck)
+  "A list of packages to ensure are installed at launch.")
+
+(defun prelude-packages-installed-p ()
+    "Check if all packages in `prelude-packages' are installed."
+      (every #'package-installed-p prelude-packages))
+
+(defun prelude-require-package (package)
+  "Install PACKAGE unless already installed."
+  (unless (package-installed-p package)
+    (package-install package)))
+
+(defun prelude-require-packages (packages)
+  "Ensure PACKAGES are installed.
+Missing packages are installed automatically."
+  (mapc #'prelude-require-package packages))
+
+(defun prelude-install-packages ()
+  "Install all packages listed in `prelude-packages'."
+  (unless (prelude-packages-installed-p)
+    ;; check for new packages (package versions)
+    (message "%s" "Emacs Prelude is now refreshing its package database...")
+    (package-refresh-contents)
+    (message "%s" " done.")
+    ;; install the missing packages
+    (prelude-require-packages prelude-packages)))
+
+(prelude-install-packages)
